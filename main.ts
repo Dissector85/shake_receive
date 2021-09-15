93924c68c6d42bbfa70531cbd975b4013b09a0e0
Dissector85 <80616305+Dissector85@users.noreply.github.com>
2021-09-15 15:14:15 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    control.waitMicros(4)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "on") {
         pins.digitalWritePin(DigitalPin.P0, 1)
